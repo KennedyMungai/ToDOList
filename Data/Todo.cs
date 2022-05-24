2a6a73fafa26775a40d64646032fa2ab070b048c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-24 12:27:57 +0300

    Set the default value for IsDone to false

--- Data/Todo.cs
+++ Data/Todo.cs
@@ -5,5 +5,5 @@ public class Todo
     public int Id { get; set; }
     public string? Item { get; set; }
     public DateTime DeadLine { get; set; }
-    public bool IsDone { get; set; }
+    public bool IsDone { get; set; } = false;
 }
