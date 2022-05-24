b786851c1e103ce92be47d05f6e5364e08ef0106
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-24 10:14:02 +0300

    Implemented to ITodoService

--- Pages/TodoService.cs
+++ Pages/TodoService.cs
@@ -1,11 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace ToDOList.Data;
 
-public class TodoService
+public class TodoService : ITodoService
 {
     private readonly TodoContext _context;
+
+    public List<Todo> todoList { get; set; } = new List<Todo>();
 
     public TodoService(TodoContext context)
     {
         _context = context;
+    }
+
+    public async Task Create(Todo todo)
+    {
+        await _context.todo.AddAsync(todo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Retrieve()
+    {
+        todoList = await _context.todo.ToListAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+        if(id != null)
+        {
+            await _context.todo.Remove(id);
+            await _context.SaveChangesAsync();
+        }
     }
 }
