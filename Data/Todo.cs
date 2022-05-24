338c4ca9d0acc7c6e7afce074b6b30be7f0adb3e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-24 10:12:32 +0300

    Added Id field

--- Data/Todo.cs
+++ Data/Todo.cs
@@ -2,6 +2,7 @@ namespace ToDOList.Data;
 
 public class Todo
 {
+    public int Id { get; set; }
     public string? Item { get; set; }
     public DateTime DeadLine { get; set; }
     public bool IsDone { get; set; }
